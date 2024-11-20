25f7dba744a8ac06d1e3c1d19e0a943c1632fc10
OtavioGhesla <otaviofetterg@gmail.com>
2024-11-20 11:13:58 -0300

    Commit do dia 20/11

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="notfound-page">
+        <h1>Página não encontrada</h1>
+    </div>
+</template>
+
+<style scoped>
+
+h1{
+    color: #000;
+    font-size: 36px;
+}
+
+</style>
